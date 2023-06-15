c6d1fa7ed6a168a5bb8cc9a5125323c87b641471
Wamaitha <wamaithaweru19@gmail.com>
2023-06-15 13:27:21 +0300

    chore: Update dependencies in tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ], 
+  theme: {
+    colors:{
+      'Orange':	'#ff7d1a',
+      'PaleOrange':	'#ffede0',
+      'VeryDarkBlue': '#1d2025',
+      'DarkGrayishBlue': '#68707d',
+      'GrayishBlue': 	'#b6bcc8',
+      'LightGrayishBlue': 	'#f7f8fd',
+      'White': '#ffff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
